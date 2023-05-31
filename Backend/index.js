b0dc4bc3648187f05cf5cffa7cb1db604c0981b6
Soumya Adhya <soumyaadhya007@gmail.com>
2023-05-31 17:13:10 +0530

    feat(server): Created Connection  MongoDB to Server

--- Backend/index.js
+++ Backend/index.js
@@ -4,8 +4,8 @@ const colors = require("colors");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
+const { connection } = require("./Config/db");
 const { APIRouter } = require("./Routers/api.router");
-
 app.use(cors());
 app.use(express.json());
 
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use("/api", APIRouter);
 
 // Start the server and listen on the specified port
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to MongoDB".bgCyan);
+  } catch (error) {}
   console.log(`Server is running on ${PORT}`.underline.yellow);
 });
